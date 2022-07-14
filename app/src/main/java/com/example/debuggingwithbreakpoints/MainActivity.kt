fb3caa72888b8e34a823f4af53b85e9f2a07b9ff
bejanandreiyps <andrei.bejan@yopeso.com>
2022-07-14 23:29:45 +0300

    Purposefully errors, his doesn't work yet

--- app/src/main/java/com/example/debuggingwithbreakpoints/MainActivity.kt
+++ app/src/main/java/com/example/debuggingwithbreakpoints/MainActivity.kt
@@ -1,11 +1,26 @@
-package com.example.debuggingwithbreakpoints
+package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.debuggingwithbreakpoints.R
+
+public val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
     }
 }
